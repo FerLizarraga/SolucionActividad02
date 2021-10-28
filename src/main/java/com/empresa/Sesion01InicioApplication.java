99aca5ff19b71cbe9d3c02ab7369412de3a492bc
Acer <Acer@LAPTOP-6Q11G36V>
2021-10-27 19:43:49 -0500

    actualizacion crud

--- src/main/java/com/empresa/Sesion01InicioApplication.java
+++ src/main/java/com/empresa/Sesion01InicioApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Sesion01InicioApplication {
-
+ 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Sesion01InicioApplication.class, args);
 	}
---
+++ src/main/java/com/empresa/entity/Alumno.java
@@ -0,0 +1,53 @@
+package com.empresa.entity;
+
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "alumno")
+@Getter
+@Setter
+
+
+public class Alumno {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	
+	private int idAlumno;
+	private String nombre;
+	private String dni;
+	private String correo;
+	
+	@JsonFormat(pattern= "yyyy-MM-dd")
+	@DateTimeFormat(pattern= "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date fechaNacimiento ;
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
